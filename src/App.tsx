95d915165172e3ba0cf98a62621e3abb7b308169
Shay Bushary <shay.bushary@1touch.io>
2023-10-21 17:14:33 +0300

    check current location of device

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
 function App() {
-  return <div>military service</div>;
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+  return (
+    <div>
+      <h2>Current location</h2>
+      {currentLocation?.latitude && (
+        <div>latitude - {currentLocation.latitude}</div>
+      )}
+      {currentLocation?.longitude && (
+        <div>longitude - {currentLocation.longitude}</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
