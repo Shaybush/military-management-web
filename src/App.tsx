f3ca26511fae413bb95b99969ca75b0a28b5f2ed
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2023-12-06 23:33:13 +0200

    Asc 41 (#2)
    * asc-42 - move app routes
    
    * asc-41 - move main.tsx
    
    * asc-43 - move index.css folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from "react";
-function App() {
-  const [currentLocation, setCurrentLocation] = useState({
-    latitude: 0,
-    longitude: 0,
-  });
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCurrentLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      });
-    }
-  }, []);
+import AppRoutes from "./AppRoutes.tsx";
+import React from "react";
+
+const App = () => {
   return (
-    <div>
-      <h2>Current location</h2>
-      {currentLocation?.latitude && (
-        <div>latitude - {currentLocation.latitude}</div>
-      )}
-      {currentLocation?.longitude && (
-        <div>longitude - {currentLocation.longitude}</div>
-      )}
-    </div>
+    <React.Fragment>
+      <AppRoutes />
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/shared/components/loaderSpinnerCircle/loaderSpinnerCircle.tsx
+++ src/shared/components/loaderSpinnerCircle/loaderSpinnerCircle.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react';
-import { ILoaderSpinnerCircleModel } from '../../models/loaderSpinnerCircle.model';
+import { FC } from "react";
+import { ILoaderSpinnerCircleModel } from "../../models/loaderSpinnerCircle.model";
 
-
-const LoaderSpinnerCircle: FC<ILoaderSpinnerCircleModel> = ({ color = 'text-dark' }) => {
+const LoaderSpinnerCircle: FC<ILoaderSpinnerCircleModel> = ({
+  color = "text-dark",
+}) => {
   return (
     <div className={`spinner-border ${color}`} role="status">
       <span className="sr-only">Loading...</span>
